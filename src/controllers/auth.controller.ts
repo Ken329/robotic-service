08a80cd0e67837663cc24ee87ae36b1c91155739
Ken329 <ken_037729@hotmail.com>
2024-05-22 22:18:04 +0800

    [fixed] wrong path for service impoprt

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,7 +4,7 @@ import {
   errorApiResponse,
   successApiResponse
 } from '../utils/helpers';
-import AuthService from '../services/Auth.service';
+import AuthService from '../services/auth.service';
 
 const generatePublicKey = (req: Request, res: Response) => {
   try {
